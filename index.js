cc295c8ab78bd33e1a8ba7a9c24403a06a9d1762
S1M0N38 <bertolottosimone@gmail.com>
2021-01-27 13:44:26 +0100

    testHeadless function

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ import { createServer } from 'http';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-const stealth = StealthPlugin();
-stealth.enabledEvasions.delete('chrome.runtime');
-stealth.enabledEvasions.delete('iframe.contentWindow');
-
 const bet365 = {
   token: undefined,
 };
-
-// other bookmaker
 
 const server = createServer((req, res) => {
   if (req.url === '/') {
@@ -25,17 +19,19 @@ server.listen(5000);
 console.log('Node.js web server at port 5000 is running..');
 
 function generateBet365Token() {
+  const stealth = StealthPlugin();
+  // stealth.enabledEvasions.delete('chrome.runtime');
+  // stealth.enabledEvasions.delete('iframe.contentWindow');
   puppeteer
     .use(stealth)
     .launch({ headless: false })
     .then(async (browser) => {
       const [page] = await browser.pages();
-      await page.setUserAgent(userAgent);
       await page.goto('https://www.bet365.com');
       await page.waitForRequest((req) => {
         if (req.url().includes('SportsBook')) {
           bet365.token = req.headers()['x-net-sync-term'];
-          console.log('bet365 token updated');
+          console.log(`${new Date()} - new token ${bet365.token}`);
           return true;
         }
       });
@@ -44,4 +40,20 @@ function generateBet365Token() {
 }
 
 generateBet365Token();
-// setInterval(generateBet365Token, 10 * 60 * 1000);
+setInterval(generateBet365Token, 10 * 60 * 1000);
+
+// function testHeadless() {
+//   const stealth = StealthPlugin();
+//   puppeteer
+//     .use(stealth)
+//     .launch({ headless: true })
+//     .then(async (browser) => {
+//       const page = await browser.newPage();
+//       await page.goto('https://bot.sannysoft.com');
+//       await page.waitFor(5000);
+//       await page.screenshot({ path: './stealth.png', fullPage: true });
+//       await browser.close();
+//     });
+// }
+
+// testHeadless();
